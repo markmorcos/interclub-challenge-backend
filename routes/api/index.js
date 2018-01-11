2d80f419091588291c2270b537a3a2ed08d40f1b
Mark Morcos <mark.yehia@gmail.com>
2018-01-11 22:11:55 +0200

    Add member details (incl. transactions) API route

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const MemberModel = require('../../models/member');
+const TransactionModel = require('../../models/transaction');
 
 router.get('/aye', (req, res) => {
     res.send('aye aye');
@@ -25,6 +26,36 @@ router.get('/list-members', (req, res) => {
         .catch(err => {
             res.status(400).send('Error');
         });
+});
+
+router.get('/members/:memberId', (req, res) => {
+    MemberModel
+        .findById(req.params.memberId)
+        .then(member => {
+            TransactionModel
+                .find({ member: req.params.memberId })
+                .sort({ date: -1 })
+                .then(transactions => {
+                    const mappedTransactions = transactions.map(transaction => {
+                        return {
+                            id: transaction._id,
+                            amount: transaction.amount,
+                            date: transaction.date,
+                            type: transaction.type
+                        };
+                    });
+                    res.json({
+                        id: member._id,
+                        first_name: member.first_name,
+                        last_name: member.last_name,
+                        number: member.number,
+                        transactions: mappedTransactions
+                    });
+                });
+            })
+            .catch(err => {
+                res.status(400).send('Error');
+            });
 });
 
 module.exports = router;
